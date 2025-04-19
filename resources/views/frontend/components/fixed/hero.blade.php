8c031c7e6ce0ea047a17e173435d30e64fae9e3f
zidankhan86 <zidankhan718@gmail.com>
2025-04-19 23:26:11 +0600

    fix:..

--- resources/views/frontend/components/fixed/hero.blade.php
+++ resources/views/frontend/components/fixed/hero.blade.php
@@ -3,7 +3,7 @@
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
             <h1 class="display-4 fw-bolder">Fake Shop</h1>
-            <p class="lead fw-normal text-white-50 mb-0">With this shop homepage</p>
+            <p class="lead fw-normal text-white-50 mb-0">We Make , We build , We Create</p>
         </div>
     </div>
 </header>
